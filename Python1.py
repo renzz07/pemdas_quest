0d2fc43e9a4716a03f825ca42882d0009a050604
renzz07 <110508883+renzz07@users.noreply.github.com>
2022-08-08 08:51:15 +0700

    Add files via upload

---
+++ Python1.py
@@ -0,0 +1,12 @@
+# Belajar Output
+
+print("Hello World")
+print("Nama Saya Raffael")
+print("Saya penyuka mobil")
+print("Saya suka nonton film")
+print("Hobby saya main sepeda")
+print("idola saya rrq lemon")
+print("cita-cita saya menjadi pengusaha")
+print("makanan favorit kwetiau goreng")
+print("minuman favorit milo")
+print("saya sekolah di smk harapan bangsa")
---
+++ python2.py
@@ -0,0 +1,13 @@
+# Belajar Input
+
+nama = "Raffael" # nama adalah Data, Raffael adalah variabel
+umur = 15 # Tipe data integer
+alamat = "Cimanggis, Kota Depok"
+
+print(nama)
+print(umur)
+print(alamat)
+
+print("Nama saya adalah " + nama)
+
+# print("Nama saya adalah " + nama + " umur saya " ) + str (umur)  + " Alamat saya " + alamat) 
