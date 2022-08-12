df34a7c8998414dc400a5624f9ad7e0c62a7fd6f
renzz07 <110508883+renzz07@users.noreply.github.com>
2022-08-12 10:49:40 +0700

    Add files via upload

---
+++ Uprak_RaffaelBintangSyahrenzyXTKJ1.py
@@ -0,0 +1,8 @@
+namadepan = input ("Masukkan Nama Depan Anda: ")
+namabelakang = input ("Masukkan Nama Belakang Anda: ")
+
+print("Nama anda adalah " , namadepan, namabelakang)
+
+suhu = int(input("Masukkan Suhu Celcius"))
+reamur = (4/5 * suhu) 
+print(suhu, "Celcius =", reamur, "reamur")
