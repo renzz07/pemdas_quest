c6b855d43c80991914d06a149c4b0ffddca63590
renzz07 <110508883+renzz07@users.noreply.github.com>
2022-08-12 10:52:30 +0700

    Add files via upload

---
+++ python3 .py
@@ -0,0 +1,8 @@
+# Belajar Input 2
+
+# Pertama kita belajar untuk input data string
+
+nama = input("Masukkan Nama Anda: ")
+nama_depan = input("Masukkan nama depan anda adalah: ")
+print("Nama anda adalah", nama)
+print("Nama depan Anda adalah", nama_depan)
---
+++ python4.py
@@ -0,0 +1,32 @@
+# Belajar Input 3
+
+# Belajar Matematika dalam program
+
+a = 5
+b = 10
+c = a + b
+
+print(a)
+print(b)
+print(c)
+
+print("a", a)
+print("b", b)
+print("c", c)
+
+
+x = int(input("masukkan Angka ke-1: "))
+y = int(input("masukkan Angka ke-2: "))
+
+print("Nilai X=",x)
+print("Nilai Y=",y)
+
+print("x + y = ", x+y)
+print("x - y =", x-y)
+print("x * y =", x*y)
+print("x / y =", x/y)
+
+suhu = int(input("Masukkan Suhu Celcius"))
+fah = (9/5 * suhu + 32)  
+print(suhu, "Celcius =", fah, "Farenheit")
+
